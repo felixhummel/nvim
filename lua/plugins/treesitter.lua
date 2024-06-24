49d8f75ca08b088c3c6b44b167f409f0c12e53a6
Felix Hummel <felix@felixhummel.de>
2024-06-24 15:13:23 +0200

    fix nvim-treesitter-textobjects

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
@@ -14,22 +17,6 @@ return {
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-
-      -- maybe
-      --
-      -- - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      -- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-    },
-    setup = {
       textobjects = {
         select = {
           enable = true,
@@ -48,12 +35,20 @@ return {
             ['@parameter.outer'] = 'v',
             ['@block.outer'] = 'V',
             ['@block.inner'] = 'V',
-            ['@function.outer'] = 'v',
-            ['@function.inner'] = 'v',
+            ['@function.outer'] = 'V',
+            ['@function.inner'] = 'V',
             ['@class.outer'] = '<c-v>',
           },
         },
       },
     },
+    config = function(_, opts)
+      require('nvim-treesitter.configs').setup(opts)
+
+      -- maybe
+      --
+      -- - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+      -- - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    end,
   },
 }
