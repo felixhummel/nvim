2c3a52d838e63a12377f8e7d7f9de582cc371dd6
Felix Hummel <felix@felixhummel.de>
2024-07-01 17:28:02 +0200

    <C-x><C-h> to complete bash history with fzf

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,3 +1,17 @@
-return {
-  'junegunn/fzf.vim', -- old-school fzf
+return { -- old-school fzf
+  {
+    'junegunn/fzf',
+    dir = '~/.fzf',
+    build = './install --key-bindings --completion --no-update-rc',
+  },
+  {
+    'junegunn/fzf.vim',
+    init = function()
+      vim.cmd([[
+" bash history completion with multiple lines
+" The "--text" flag lets rg continue even if a NUL byte is found
+inoremap <expr> <c-x><c-h> fzf#vim#complete({ 'source': 'rg --text -v ^# ~/.bash_history', 'options': '--multi --tac --no-sort --exact', 'reducer': { lines -> join(lines, "\n") }})
+]])
+    end,
+  },
 }
