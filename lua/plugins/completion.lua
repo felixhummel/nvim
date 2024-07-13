d02b07bf922a833b2b71ff45249231d9ef1c28a8
Felix Hummel <felix@felixhummel.de>
2024-07-13 23:51:08 +0200

    cleanup completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,33 +1,15 @@
-return { -- Autocompletion
+return {
   'hrsh7th/nvim-cmp',
   event = 'InsertEnter',
   dependencies = {
-    -- Snippet Engine & its associated nvim-cmp source
     {
       'L3MON4D3/LuaSnip',
       build = (function()
-        -- Build Step is needed for regex support in snippets.
-        -- This step is not supported in many windows environments.
-        -- Remove the below condition to re-enable on windows.
-        if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-          return
-        end
         return 'make install_jsregexp'
       end)(),
       config = function()
         require('luasnip.loaders.from_snipmate').lazy_load()
       end,
-      dependencies = {
-        -- `friendly-snippets` contains a variety of premade snippets.
-        --    See the README about individual language/framework/plugin snippets:
-        --    https://github.com/rafamadriz/friendly-snippets
-        -- {
-        --   'rafamadriz/friendly-snippets',
-        --   config = function()
-        --     require('luasnip.loaders.from_vscode').lazy_load()
-        --   end,
-        -- },
-      },
     },
     'saadparwaiz1/cmp_luasnip',
     'hrsh7th/cmp-buffer',
@@ -85,7 +67,7 @@ return { -- Autocompletion
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         -- https://www.reddit.com/r/neovim/comments/xrbdny/how_to_select_from_nvimcmp_only_after_having/
-        ['<CR>'] = cmp.mapping.confirm { select = false },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
         ['<C-Space>'] = cmp.mapping.complete {},
       },
       sources = {
