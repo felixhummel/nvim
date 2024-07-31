9420eb6e2790210ec17d243a7e01c51c14816e52
Felix Hummel <felix@felixhummel.de>
2024-08-01 01:51:57 +0200

    update which-key bindings for 3.0

--- lua/config/which-key.lua
+++ lua/config/which-key.lua
@@ -1,18 +1,20 @@
-local which_key = require 'which-key'
+local which_key = require('which-key')
 which_key.setup()
 
 -- normal
-which_key.register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-  ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-}
+which_key.add({
+  { '<leader>c', group = '[C]ode' },
+  { '<leader>d', group = '[D]ocument' },
+  { '<leader>h', group = 'Git [H]unk' },
+  { '<leader>r', group = '[R]ename' },
+  { '<leader>s', group = '[S]earch' },
+  { '<leader>t', group = '[T]oggle' },
+  { '<leader>w', group = '[W]orkspace' },
+})
 
 -- visual
-which_key.register({
-  ['<leader>h'] = { 'Git [H]unk' },
-}, { mode = 'v' })
+which_key.add({
+  '<leader>h',
+  desc = 'Git [H]unk',
+  mode = 'v',
+})
