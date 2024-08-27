394139023a09dd4bb263f116235436a2054ec13f
Felix Hummel <felix@felixhummel.de>
2024-08-27 10:47:14 +0200

    refac: move config/x into plugin/x
    This way, Lazy can reload the plugin. Nothing for me to do.
    Lazy is really hard-working for me, despite its name. :D

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,6 +2,25 @@ return { -- Useful plugin to show you pending keybinds.
   'folke/which-key.nvim',
   event = 'VimEnter',
   config = function()
-    require 'config.which-key'
+    local which_key = require('which-key')
+    which_key.setup()
+
+    -- normal
+    which_key.add({
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>h', group = '[H]elp' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>w', group = '[W]orkspace' },
+    })
+
+    -- visual
+    which_key.add({
+      '<leader>h',
+      desc = 'Git [H]unk',
+      mode = 'v',
+    })
   end,
 }
