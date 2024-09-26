b4477b8d58ae6f8465741013ae10590a4b22b118
Felix Hummel <felix@felixhummel.de>
2024-09-26 10:33:48 +0200

    oil: remain in jumplist for <C-o>

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,8 @@
 return {
   'stevearc/oil.nvim',
-  config = function()
-    require 'config.oil'
-  end,
+  opts = {
+    -- C-o https://github.com/stevearc/oil.nvim/issues/201
+    cleanup_delay_ms = false,
+  },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
