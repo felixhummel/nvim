e620d70a8b09e62587e8b35229b866d469605680
Felix Hummel <felix@felixhummel.de>
2024-10-21 09:12:55 +0200

    plugin setup in plugins/

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
   opts = {
     -- C-o https://github.com/stevearc/oil.nvim/issues/201
     cleanup_delay_ms = false,
+    view_options = {
+      show_hidden = true,
+    },
   },
   dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
