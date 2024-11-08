4cd7e051bd8ce070bf75ebe72e9971752d451e9b
Felix Hummel <felix@felixhummel.de>
2024-11-08 11:33:09 +0100

    remove nvim-cmp and use mini.completion
    I got tired of weird behaviour in command mode (`:`), e.g.:
    
      :e <Tab>
    
    Resulted in ^I instead of file name completion.
    After removing nvim-cmp, command mode completion worked as expected
    again.
    But I still want LSP completions. That's where mini.completion comes
    into play.

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -82,8 +82,9 @@ return {
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+    local capabilities = {}
     --  Add any additional override configuration in the following tables. Available keys are:
     --  - cmd (table): Override the default command used to start the server
     --  - filetypes (table): Override the default list of associated filetypes for the server
