6bbda765b053c5b83b47cc8a0a11a551b46ab828
Felix Hummel <felix@felixhummel.de>
2025-03-07 10:44:31 +0100

    use mason-lock instead of mason-tool-installer

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ return {
     -- Automatically install LSPs and related tools to stdpath for Neovim
     { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
     'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
+    'zapling/mason-lock.nvim',
     { 'j-hui/fidget.nvim', opts = {} },
 
     -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
@@ -82,9 +82,9 @@ return {
     --  By default, Neovim doesn't support everything that is in the LSP specification.
     --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
     --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
-    -- local capabilities = vim.lsp.protocol.make_client_capabilities()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
     -- capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-    local capabilities = {}
+    -- local capabilities = {}
     --  Add any additional override configuration in the following tables. Available keys are:
     --  - cmd (table): Override the default command used to start the server
     --  - filetypes (table): Override the default list of associated filetypes for the server
@@ -93,7 +93,6 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       basedpyright = {},
-      rust_analyzer = {},
       -- This language service will emit VS Code-specific telemetry events. If using the service outside of VS Code (e.g. in Vim), these telemetry events can be safely ignored.
       -- https://github.com/microsoft/compose-language-service#telemetry
       -- The following must be set for docker compose files: filetype=yaml.docker-compose
@@ -115,15 +114,15 @@ return {
     -- :Mason
     require('mason').setup()
 
-    -- You can add other tools here that you want Mason to install
-    -- for you, so that they are available from within Neovim.
-    local ensure_installed = vim.tbl_keys(servers or {})
-    vim.list_extend(ensure_installed, {
-      'stylua',
+    -- :MasonLock
+    -- :MasonLockRestore
+    require('mason-lock').setup({
+      lockfile_path = vim.fn.stdpath('config') .. '/mason-lock.json', -- (default)
     })
-    require('mason-tool-installer').setup({ ensure_installed = ensure_installed })
 
     require('mason-lspconfig').setup({
+      ensure_installed = {}, -- I use :MasonLock to install servers
+      automatic_installation = false,
       handlers = {
         function(server_name)
           local server = servers[server_name] or {}
