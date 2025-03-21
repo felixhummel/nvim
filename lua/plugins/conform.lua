191c45843081c42702b3e65231e8ccea99bba665
Felix Hummel <felix@felixhummel.de>
2025-03-21 09:03:23 +0100

    conform: toggle format

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,4 @@
+-- FormatEnable / FormatDisable from https://github.com/stevearc/conform.nvim/issues/192#issuecomment-2573170631
 return {
   'stevearc/conform.nvim',
   lazy = false,
@@ -18,12 +19,46 @@ return {
       mode = '',
       desc = '[F]ormat buffer',
     },
+    {
+      '<leader>vf',
+      function()
+        -- If autoformat is currently disabled for this buffer,
+        -- then enable it, otherwise disable it
+        if vim.b.disable_autoformat then
+          vim.cmd('FormatEnable')
+          vim.notify('Enabled autoformat for current buffer')
+        else
+          vim.cmd('FormatDisable!')
+          vim.notify('Disabled autoformat for current buffer')
+        end
+      end,
+      desc = 'Toggle autoformat for current buffer',
+    },
+    {
+      '<leader>vF',
+      function()
+        -- If autoformat is currently disabled globally,
+        -- then enable it globally, otherwise disable it globally
+        if vim.g.disable_autoformat then
+          vim.cmd('FormatEnable')
+          vim.notify('Enabled autoformat globally')
+        else
+          vim.cmd('FormatDisable')
+          vim.notify('Disabled autoformat globally')
+        end
+      end,
+      desc = 'Toggle autoformat globally',
+    },
   },
   ---@module "conform"
   ---@type conform.setupOpts
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
+      -- respect disable flag
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+        return
+      end
       -- disable docker-compose.yml auto format
       local bufname = vim.api.nvim_buf_get_name(0)
       local pattern = vim.glob.to_lpeg('docker-compose*.y*ml')
@@ -71,4 +106,27 @@ return {
       },
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- :FormatDisable! disables autoformat for this buffer only
+        vim.b.disable_autoformat = true
+      else
+        -- :FormatDisable disables autoformat globally
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true, -- allows the ! variant
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+  end,
 }
