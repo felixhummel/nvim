d5a6193ef0cf542f6e98888cac10c8565d15a3bb
Felix Hummel <felix@felixhummel.de>
2025-08-06 15:36:09 +0200

    dadbod: always save queries

--- lua/plugins/vim-dadbod-ui.lua
+++ lua/plugins/vim-dadbod-ui.lua
@@ -12,6 +12,8 @@ return {
   },
   init = function()
     vim.g.db_ui_use_nerd_fonts = 1
+    vim.g.db_ui_tmp_query_location = '~/.local/share/db_ui/queries'
+    vim.g.db_ui_show_database_icon = 1
     vim.cmd('autocmd FileType sql setlocal omnifunc=vim_dadbod_completion#omni')
   end,
 }
