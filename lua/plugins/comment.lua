e73e14eef161b622bd1af49100fb6ff106e9aa7e
Felix Hummel <felix@felixhummel.de>
2025-08-29 22:30:54 +0200

    C-/ (comment) mapping wezterm style

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,10 +4,10 @@ return {
     -- ~/.local/share/nvim/lazy/Comment.nvim/lua/Comment/init.lua
     -- bind Ctrl-/ to comment toggle
     toggler = {
-      line = '<C-_>',
+      line = '<C-/>', -- use '<C-_>' for old terminals
     },
     opleader = {
-      line = '<C-_>',
+      line = '<C-/>', -- use '<C-_>' for old terminals
     },
   },
 }
